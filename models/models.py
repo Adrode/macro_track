9f73484f9e7d3b17707af23aafc344500551ea46
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-04-16 17:48:20 +0200

    feature: realtionshiprs

--- models/models.py
+++ models/models.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from typing import Optional
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 
 class Base(DeclarativeBase):
   pass
@@ -11,3 +13,53 @@ class User(Base):
   email: Mapped[str] = mapped_column(unique=True)
   username: Mapped[Optional[str]]
   hashed_password: Mapped[str]
+  kcal_daily_goal: Mapped[int]
+  protein_daily_goal: Mapped[int]
+  fat_daily_goal: Mapped[int]
+  carbs_daily_goal: Mapped[int]
+
+  meals: Mapped[list["Meal"]] = relationship()
+  diary: Mapped["UserDiary"] = relationship()
+
+class Product(Base):
+  __tablename__ = "products"
+
+  id: Mapped[int] = mapped_column(primary_key=True)
+  category: Mapped[str]
+  name: Mapped[str]
+  kcal_per_100g: Mapped[int]
+  protein_per_100g: Mapped[int]
+  fat_per_100g: Mapped[int]
+  carbs_per_100g: Mapped[int]
+
+class Meal(Base):
+  __tablename__ = "meals"
+
+  id: Mapped[int] = mapped_column(primary_key=True)
+  category: Mapped[str]
+  name: Mapped[str]
+  user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+  meals_products: Mapped["MealProduct"] = relationship()
+
+class MealProduct(Base):
+  __tablename__ = "meals_products"
+
+  id: Mapped[int] = mapped_column(primary_key=True)
+  meal_id: Mapped[int] = mapped_column(ForeignKey("meals.id"))
+  product_id: Mapped[int] = mapped_column(ForeignKey("products.id"))
+  grams: Mapped[int]
+
+  product: Mapped["Product"] = relationship()
+
+class UserDiary(Base):
+  __tablename__ = "users_diary"
+
+  id: Mapped[int] = mapped_column(primary_key=True)
+  user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+  meal_id: Mapped[int] = mapped_column(ForeignKey("meals.id"))
+  meal_datetime: Mapped[datetime]
+
+  meal: Mapped[list["Meal"]] = relationship()
+
+# do dokończenia jeszcze relacje (back_populates) 
