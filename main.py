7ac3ef64df52d71386a14d3092ebe2a552cfcf8c
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-04-19 10:51:51 +0200

    fix: register endpoint moved to /auth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from routes import users
+from routes import users, auth
 
 app = FastAPI()
 
+app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
--- routes/users.py
+++ routes/users.py
@@ -1,39 +1,8 @@
-from fastapi import APIRouter, HTTPException
-from sqlalchemy.exc import IntegrityError
-from schemas import schemas
-from models import models
+from fastapi import APIRouter
 from utils.dependencies import session_dependency
-from authentication.pwd_hash import hash_password
 
 router = APIRouter()
 
 @router.get("/me")
 def me(db: session_dependency):
   pass
-
-
-# to refactor later to authentication
-# ---
-@router.post("/register", response_model=schemas.ResponseUser)
-def register(data: schemas.CreateUser, db: session_dependency):
-  try:
-    new_user = models.User(
-      email=data.email,
-      username=data.username,
-      hashed_password=hash_password(data.password),
-      kcal_daily_goal=data.kcal_daily_goal,
-      protein_daily_goal=data.protein_daily_goal,
-      fat_daily_goal=data.fat_daily_goal,
-      carbs_daily_goal=data.carbs_daily_goal
-    )
-
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
-  except IntegrityError:
-    raise HTTPException(
-      status_code=400,
-      detail="Bad request"
-    )
-# ---
