af4f4cdf2f69f841f3268391d79613f6329fb743
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-04-24 16:57:41 +0200

    alembic init

--- database/database.py
+++ database/database.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from models.models import Base
 
 engine = create_engine('postgresql+psycopg2://macro_track_user:macro_track_passwd@localhost:5432/macro_track_db', echo=True)
-
-Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
