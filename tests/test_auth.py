16beeb2c828032223772703e1ef32354dd8a4012
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-05-31 14:58:34 +0200

    fix: refactor fixtures for tests - moved to conftest

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,29 +1,3 @@
-import pytest, json
-from uuid import uuid4
-from models.models import User
-from authentication.pwd_hash import hash_password
-
-@pytest.fixture()
-def test_user(db_session):
-  password = "fakehash"
-
-  user = User(
-    email=f"adrian-{uuid4()}@gmail.com",
-    username="Adrian",
-    hashed_password=hash_password(password),
-    kcal_daily_goal=2000,
-    protein_daily_goal=100,
-    fat_daily_goal=70,
-    carbs_daily_goal=250
-  )
-  db_session.add(user)
-  db_session.commit()
-  db_session.refresh(user)
-
-  user.plain_password = password
-
-  return user
-
 def test_register_valid_data(client):
   response = client.post(
     "/auth/register",
@@ -59,10 +33,10 @@ def test_register_invalid_email(client):
   assert response.status_code == 422
 
 def test_register_duplicated_email(client):
-  response = client.post(
+  response1 = client.post(
     "/auth/register",
     json={
-      "email": f"adison@gmail.com",
+      "email": "adison@gmail.com",
       "username": "Nosida",
       "password": "hashfake",
       "kcal_daily_goal": 1500,
@@ -72,14 +46,28 @@ def test_register_duplicated_email(client):
     }
   )
 
-  assert response.status_code == 400
+  response2 = client.post(
+    "/auth/register",
+    json={
+      "email": "adison@gmail.com",
+      "username": "Nosida",
+      "password": "hashfake",
+      "kcal_daily_goal": 1500,
+      "protein_daily_goal": 100,
+      "fat_daily_goal": 70,
+      "carbs_daily_goal": 150
+    }
+  )
 
-def test_login_valid_data(client, test_user):
+  assert response1.status_code == 200
+  assert response2.status_code == 400
+
+def test_login_valid_data(client, test_first_user):
   response = client.post(
     "/auth/login",
     data={
-      "username": test_user.email,
-      "password": test_user.plain_password
+      "username": test_first_user.email,
+      "password": test_first_user.plain_password
     }
   )
 
@@ -97,11 +85,11 @@ def test_login_invalid_data(client):
 
   assert response.status_code == 401
 
-def test_login_invalid_password(client, test_user):
+def test_login_invalid_password(client, test_first_user):
   response = client.post(
     "/auth/login",
     data={
-      "username": test_user.email,
+      "username": test_first_user.email,
       "password": "kekw"
     }
   )
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,32 +1,13 @@
-import pytest
-from uuid import uuid4
 import authentication.short_tokens as auth
-from models.models import User
-
-@pytest.fixture()
-def test_user(db_session):
-  user = User(
-    email=f"adrian={uuid4()}@gmail.com",
-    username="Adrian",
-    hashed_password="fakehash",
-    kcal_daily_goal=2000,
-    protein_daily_goal=100,
-    fat_daily_goal=70,
-    carbs_daily_goal=250
-  )
-  db_session.add(user)
-  db_session.commit()
-  db_session.refresh(user)
-  return user
 
 def test_get_me_unauthorized(client):
   response = client.get("/users/")
 
   assert response.status_code == 401
 
-def test_get_me_authorized(client, test_user):
+def test_get_me_authorized(client, test_first_user):
   token = auth.create_access_token({
-      "sub": test_user.email
+      "sub": test_first_user.email
     })
   response = client.get(
     "/users/",
@@ -39,17 +20,17 @@ def test_get_me_authorized(client, test_user):
   assert "email" in data
   assert "id" in data
   
-def test_patch_me_unauthorized(client, test_user):
+def test_patch_me_unauthorized(client, test_first_user):
   response = client.patch(
     "/users/",
-    json={"username": test_user.username}
+    json={"username": test_first_user.username}
   )
 
   assert response.status_code == 401
 
-def test_patch_me_authorized(client, test_user):
+def test_patch_me_authorized(client, test_first_user):
   token = auth.create_access_token({
-      "sub": test_user.email
+      "sub": test_first_user.email
     })
   response = client.patch(
     "/users/",
@@ -66,9 +47,9 @@ def test_patch_me_authorized(client, test_user):
 
   assert response2.json()["username"] == "Adrian"
 
-def test_patch_me_invalid_email(client, test_user):
+def test_patch_me_invalid_email(client, test_first_user):
   token = auth.create_access_token({
-      "sub": test_user.email
+      "sub": test_first_user.email
     })
   response = client.patch(
     "/users/",
