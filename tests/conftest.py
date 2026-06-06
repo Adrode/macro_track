d34909cf1d135c3543532e7da3be27794d07c4e0
Adrian Wozniak <adrianwozniak20@gmail.com>
2026-06-06 19:43:04 +0200

    fix: comments removed

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,11 +11,8 @@ from authentication.pwd_hash import hash_password
 
 engine = create_engine(
   "sqlite:///:memory:",
-    # używa SQLite i trzyma bazę tylko w RAM, przez co jest szybko i baza znika po zakończeniu testów
   connect_args={"check_same_thread": False},
-    # SQLite domyślnie robi jedno połączenie = jeden wątek, a TestClient/FastAPI mogą używać różnych threads, więc to pozwala na jedno połączenie
   poolclass=StaticPool
-    # normalnie każde połączenie = nowa pusta baza, a ten zapis trzyma jedno połączenie "przy życiu"
 )
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -28,20 +25,12 @@ def override_get_db():
     session.close()
 
 app.dependency_overrides[get_db] = override_get_db
-  # to powoduje przy testach użycie dla endpointów testowej DB, a nie produkcyjnej
 
-@pytest.fixture(scope="session", autouse=True) # fixture to dane przygotowane pod testy
+@pytest.fixture(scope="session", autouse=True)
 def setup_db():
-    Base.metadata.create_all(bind=engine) # przed testami tworzy bazę
+    Base.metadata.create_all(bind=engine)
     yield
-    Base.metadata.drop_all(bind=engine) # po testach zamyka bazę
-  # scope="session" uruchamia ten setup dla całego pytest run, czyli:
-  #   pytest start
-  #   create tables
-  #   wszystkie uruchomione testy
-  #   pytest end
-  #   drop tables
-  # autouse=True czyli automatycznie używa setup dla każdego testu, więc nie trzeba tego pisać w arugmencie każdego testu
+    Base.metadata.drop_all(bind=engine) 
 
 @pytest.fixture()
 def db_session():
@@ -54,11 +43,6 @@ def db_session():
 @pytest.fixture()
 def client():
   return TestClient(app)
-  # TestClient to po prostu fake client HTTP, czyli:
-  #   nie odpala uvicorna
-  #   nie otwiera portu
-  #   nie ma prawdziwego networkingu
-  # ale FastAPI zachowuje się tak samo jak przy prawdziwym request
 
 @pytest.fixture(autouse=True)
 def clean_db(db_session):
