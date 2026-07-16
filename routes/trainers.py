9e201a02b013df0a8516aaabd688bc440637bb8a
Adrode <adrianwozniak20@gmail.com>
2026-07-16 16:49:48 +0200

    feature: all trainers endpoint

--- routes/trainers.py
+++ routes/trainers.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from sqlalchemy import select
 from models import models
 from schemas import trainer_schemas
-from utils.dependencies import session_dependency, current_trainer_dependency
+from utils.dependencies import session_dependency, current_trainer_dependency, current_user_dependency
 from utils.exceptions import not_authorized_token_exc
 
 router = APIRouter()
@@ -15,3 +15,12 @@ def get_me(session: session_dependency, current_trainer: current_trainer_depende
         raise not_authorized_token_exc("Not authorized")
     
     return trainer
+
+@router.get("/all", response_model=list[trainer_schemas.TrainerReponse])
+def get_all_trainers(session: session_dependency, get_current_user: current_user_dependency):
+    trainers = session.scalars(select(models.Trainer)).all()
+
+    if not trainers:
+        raise not_authorized_token_exc("Not authorized")
+    
+    return trainers
