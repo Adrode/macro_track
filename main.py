80cd62d5b0cdbd84f8a01151be7c5fa3bbe202fe
Adrode <adrianwozniak20@gmail.com>
2026-07-20 18:44:04 +0200

    feature: connections logic moved to connections.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routes import users, auth, products, meals, diary, ai, trainers
+from routes import users, auth, products, meals, diary, ai, trainers, connections
 
 app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(ai.router, prefix="/ai", tags=["AI"])
 app.include_router(trainers.router, prefix="/trainers", tags=["Trainers"])
+app.include_router(connections.router, prefix="/connections", tags=["Connections"])
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(products.router, prefix="/products", tags=["Products"])
 app.include_router(meals.router, prefix="/meals", tags=["Meals"])
