0f1039a19348a9dde557b52476f919e9c3afb6cd
Adrode <adrianwozniak20@gmail.com>
2026-07-23 18:44:56 +0200

    fix: models diaries fixed to work as snapshots

--- models/models.py
+++ models/models.py
@@ -20,7 +20,7 @@ class User(Base):
 
   meals: Mapped[list["Meal"]] = relationship(back_populates="user", passive_deletes=True)
   products: Mapped[list["Product"]] = relationship(back_populates="user", passive_deletes=True)
-  diary: Mapped[list["UserDiary"]] = relationship(back_populates="user", passive_deletes=True)
+  diary: Mapped[list["DiaryEntry"]] = relationship(back_populates="user", passive_deletes=True)
   ai_messages: Mapped[list["AIDetails"]] = relationship(back_populates="user", passive_deletes=True)
   trainer_connection: Mapped[list["TrainerClientConnection"]] = relationship(back_populates="client")
 
@@ -33,6 +33,7 @@ class Trainer(Base):
   hashed_password: Mapped[str]
 
   client_connection: Mapped[list["TrainerClientConnection"]] = relationship(back_populates="trainer")
+  products: Mapped[list["Product"]] = relationship(back_populates="trainer", passive_deletes=True)
 
 class TrainerClientConnection(Base):
   __tablename__  = "trainer_client"
@@ -59,8 +60,10 @@ class Product(Base):
   fat_per_100g: Mapped[int]
   carbs_per_100g: Mapped[int]
   user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), nullable=True)
+  trainer_id: Mapped[int] = mapped_column(ForeignKey("trainers.id", ondelete="CASCADE"), nullable=True)
 
   user: Mapped["User"] = relationship(back_populates="products")
+  trainer: Mapped["Trainer"] = relationship(back_populates="products")
 
 class Meal(Base):
   __tablename__ = "meals"
@@ -72,10 +75,10 @@ class Meal(Base):
   is_active: Mapped[bool] = mapped_column(default=True)
 
   user: Mapped["User"] = relationship(back_populates="meals")
-  meals_products: Mapped[list["MealProduct"]] = relationship(passive_deletes=True)
+  meal_products: Mapped[list["MealProduct"]] = relationship(passive_deletes=True)
 
 class MealProduct(Base):
-  __tablename__ = "meals_products"
+  __tablename__ = "meal_products"
 
   id: Mapped[int] = mapped_column(primary_key=True)
   meal_id: Mapped[int] = mapped_column(ForeignKey("meals.id", ondelete="CASCADE"))
@@ -84,16 +87,28 @@ class MealProduct(Base):
 
   product: Mapped["Product"] = relationship()
 
-class UserDiary(Base):
-  __tablename__ = "users_diary"
+class DiaryEntry(Base):
+  __tablename__ = "diaries_entry"
 
   id: Mapped[int] = mapped_column(primary_key=True)
   user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
-  meal_id: Mapped[int] = mapped_column(ForeignKey("meals.id"))
+  meal_category: Mapped[str]
+  meal_name: Mapped[str] 
   meal_datetime: Mapped[datetime]
 
-  meal: Mapped["Meal"] = relationship()
   user: Mapped["User"] = relationship(back_populates="diary")
+  diary_meal_products: Mapped[list["DiaryMealProduct"]] = relationship(passive_deletes=True)
+
+class DiaryMealProduct(Base):
+  __tablename__ = "diaries_meal_products"
+
+  id: Mapped[int] = mapped_column(primary_key=True)
+  diary_id: Mapped[int] = mapped_column(ForeignKey("diaries_entry.id", ondelete="CASCADE"))
+  name: Mapped[str]
+  kcal_per_100g: Mapped[int]
+  protein_per_100g: Mapped[int]
+  fat_per_100g: Mapped[int]
+  carbs_per_100g: Mapped[int]
 
 class AIDetails(Base):
   __tablename__ = "ai_details"
@@ -106,3 +121,13 @@ class AIDetails(Base):
   user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
 
   user: Mapped["User"] = relationship(back_populates="ai_messages")
+
+
+# TRZEBA ZROBIĆ USERS_DIARY NA ZASADZIE SNAPSHOTÓW, Z WŁASNYM DIARY_MEAL_PRODUCTS (też na snapshotach)
+# MEAL I MEAL_PRODUCTS POZOSTAJĄ BEZ ZMIAN, PONIEWAŻ TO SĄ SZABLONY
+
+# TEMPLATES mealsów dla trenera? jako snapshot samych nazw produktów z których składać ma się meals?
+
+# TRENER MA WŁASNE PRODUKTY, a jak używa tego produktu dla meala który dodaje użytkownikowi, to dla tego usera w tabeli
+# musi stworzyć się kopia tego produktu; najlepiej w formie trainer_id w products i tworzyć kopię; wtedy albo user_id albo trainer_id dla
+# jednego wpisu musi być Null, bo ten sam rekord nie może mieć ownera trainer i user jednocześnie, ponieważ ma się to opierać na kopiach
