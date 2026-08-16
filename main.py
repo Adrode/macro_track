90f7bb17ef60df82737c3d3dace299d29df70b45
Adrode <adrianwozniak20@gmail.com>
2026-08-16 11:39:13 +0200

    feature: exercises get endpoints

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from fastapi import FastAPI
 from routes.trainers import trainers_auth, trainers_connections, trainers, trainers_products, trainers_meals_for_users
-from routes.users import users_auth, users_ai, users_connections, users, users_diary, users_meals, users_products, users_training_plans
+from routes.users import users_auth, users_ai, users_connections, users, users_diary, users_exercises, users_meals, users_products
 
 app = FastAPI()
 
-app.include_router(users_auth.router, prefix="/auth/user", tags=["Users auth"])
-app.include_router(trainers_auth.router, prefix="/auth/trainer", tags=["Trainers auth"])
-app.include_router(users_ai.router, prefix="/ai", tags=["Users AI"])
-app.include_router(trainers.router, prefix="/trainer", tags=["Trainers"])
-app.include_router(trainers_connections.router, prefix="/trainer/connections", tags=["Trainers connections"])
-app.include_router(trainers_products.router, prefix="/trainer/products", tags=["Trainers products"])
-app.include_router(trainers_meals_for_users.router, prefix="/trainer/meals", tags=["Trainers meals for users"])
-app.include_router(users.router, prefix="/user", tags=["Users"])
-app.include_router(users_connections.router, prefix="/user/connections", tags=["Users connections"])
-app.include_router(users_products.router, prefix="/user/products", tags=["Users products"])
-app.include_router(users_meals.router, prefix="/user/meals", tags=["Users meals"])
-app.include_router(users_diary.router, prefix="/user/diary", tags=["Users diary"])
-app.include_router(users_training_plans.router, prefix="/user/training_plan", tags=["Users training plans"])
+app.include_router(users_auth.router, prefix="/auth/user", tags=["User auth"])
+app.include_router(trainers_auth.router, prefix="/auth/trainer", tags=["Trainer auth"])
+app.include_router(users_ai.router, prefix="/ai", tags=["User AI"])
+app.include_router(trainers.router, prefix="/trainer", tags=["Trainer"])
+app.include_router(trainers_connections.router, prefix="/trainer/connections", tags=["Trainer connections"])
+app.include_router(trainers_products.router, prefix="/trainer/products", tags=["Trainer products"])
+app.include_router(trainers_meals_for_users.router, prefix="/trainer/meals", tags=["Trainer meals for users"])
+app.include_router(users.router, prefix="/user", tags=["User"])
+app.include_router(users_connections.router, prefix="/user/connections", tags=["User connections"])
+app.include_router(users_products.router, prefix="/user/products", tags=["User products"])
+app.include_router(users_meals.router, prefix="/user/meals", tags=["User meals"])
+app.include_router(users_diary.router, prefix="/user/diary", tags=["User diary"])
+app.include_router(users_exercises.router, prefix="/user/exercises", tags=["User exercises"])
---
+++ routes/users/users_exercises.py
@@ -0,0 +1,70 @@
+from fastapi import APIRouter
+from sqlalchemy import select, or_, and_
+from models import models
+from schemas import exercise_schemas
+from utils.dependencies import session_dependency, current_user_dependency
+from utils.exceptions import not_authorized_token_exc
+
+router = APIRouter()
+
+@router.post("/", response_model=exercise_schemas.ExerciseResponse)
+def create_exercise(
+    data: exercise_schemas.CreateExercise,
+    session: session_dependency,
+    current_user: current_user_dependency
+):
+    new_exercise = models.Exercise(
+        name = data.name,
+        main_muscles = data.main_muscles,
+        side_muscles = data.side_muscles,
+        description = data.description,
+        user_id = current_user.id
+    )
+
+    session.add(new_exercise)
+    session.commit()
+    session.refresh(new_exercise)
+
+    return new_exercise
+
+@router.get("/{id}", response_model=exercise_schemas.ExerciseResponse)
+def get_exercise(
+    id: int,
+    session: session_dependency,
+    current_user: current_user_dependency
+):
+    exercise = session.scalars(select(models.Exercise).where(
+        models.Exercise.id == id,
+        or_(
+            models.Exercise.user_id == current_user.id,
+            and_(
+                models.Exercise.user_id == None,
+                models.Exercise.trainer_id == None
+            )
+        )
+    )).first()
+
+    if not exercise:
+        raise not_authorized_token_exc("Not authorized")
+
+    return exercise
+
+@router.get("/", response_model=list[exercise_schemas.ExerciseResponse])
+def get_exercises(
+    session: session_dependency,
+    current_user: current_user_dependency
+):
+    exercises = session.scalars(select(models.Exercise).where(
+        or_(
+            models.Exercise.user_id == current_user.id,
+            and_(
+                models.Exercise.user_id == None,
+                models.Exercise.trainer_id == None
+            )
+        )
+    )).all()
+
+    if not exercises:
+        raise not_authorized_token_exc("Not authorized")
+
+    return exercises
